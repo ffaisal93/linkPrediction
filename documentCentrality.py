9799b227e90efff917fd730b59fde32c06f2723a
faisal <fahimfaisal@iut-dhaka.edu>
2018-12-17 17:27:28 +0600

    variable length argument added

--- documentCentrality.py
+++ documentCentrality.py
@@ -1,20 +1,26 @@
 import numpy as np
 
-def DocumentCentrality(TD, n_it):
-    n_it = 20
-    TD = np.array(TD)
-    N, T = np.shape(TD)
+
+def document_centrality(td, *args):
+    if len(args) > 0:
+        n_it = args[0]
+    else:
+        n_it = 20
+    if n_it < 2:
+        n_it = 20
+    td = np.array(td)
+    N, T = np.shape(td)
     if N > 1 and T > 1:
         l = np.zeros((N, n_it + 1))
         k = np.zeros((T, n_it + 1))
-        k0 = TD.sum(axis=0)
-        l0 = TD.sum(axis=1)
+        k0 = td.sum(axis=0)
+        l0 = td.sum(axis=1)
         k[:, 0] = k0
         l[:, 0] = l0
 
         for i in range(1, n_it + 1):
-            k[:, i] = (1 / k0).transpose() * TD.transpose().dot(l[:, i - 1])
-            l[:, i] = (1 / l0) * TD.dot(k[:, i - 1])
+            k[:, i] = (1 / k0).transpose() * td.transpose().dot(l[:, i - 1])
+            l[:, i] = (1 / l0) * td.dot(k[:, i - 1])
         x1 = k[:, 0]
         x1[x1 == 0] = np.nan
         x1 = np.log(x1)
@@ -39,7 +45,7 @@ def DocumentCentrality(TD, n_it):
 
 
 # if __name__ == '__main__':
-#     TD = [[1, 2, 3, 4], [2, 3, 4, 5], [1, 2, 7, 8], [3, 5, 1, 2]]
-#     TD = [[1, 2, 3], [4, 5, 6], [3, 5, 7], [7, 1, 2], [3, 6, 4], [3, 5, 5]]
-#     Cd, Ct = DocumentCentrality(TD, 20)
+#     td = [[1, 2, 3, 4], [2, 3, 4, 5], [1, 2, 7, 8], [3, 5, 1, 2]]
+#     td = [[1, 2, 3], [4, 5, 6], [3, 5, 7], [7, 1, 2], [3, 6, 4], [3, 5, 5]]
+#     Cd, Ct = document_centrality(td, 20)
 #     print(Cd,Ct)
